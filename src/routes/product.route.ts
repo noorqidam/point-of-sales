4143c4b874a52dea1b54e12089248c0f144e93bd
noorqidam <noor.mujahidin@idstar.co.id>
2024-08-07 08:51:58 +0700

    feat(product): adjustment route

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -13,7 +13,7 @@ const router = express.Router();
 
 router.post("/", create_product_validation(), isAuthenticated, create_product);
 router.get("/:id", isAuthenticated, get_product);
-router.put("/:id", isAuthenticated, update_product);
+router.patch("/:id", isAuthenticated, update_product);
 router.delete("/:id", isAuthenticated, delete_product);
 router.get("/", isAuthenticated, list_products);
 
